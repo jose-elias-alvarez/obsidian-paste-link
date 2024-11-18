1918151e66cb524090cc7622fe1a34b62ceac90e
Jose Alvarez <jealv@icloud.com>
2024-11-17 19:30:18 -0500

    feat: add special URL handling

--- README.md
+++ README.md
@@ -52,7 +52,7 @@ Additionally, URLs containing newlines are not handled, since Obsidian doesn't s
 
 If the `Fetch page titles on paste` setting is enabled, the plugin will attempt to fetch page titles from HTTP URLs and use them as titles when `Override paste handler` is enabled. Alternatively, you can also fetch page titles on demand using the `Paste link and fetch page title` command.
 
-Note that titles from some pages (e.g. Reddit) may fail to load. The priority here is speed and predictability, not absolute correctness. If you rely heavily on this functionality, I recommend [obsidian-auto-link-title](https://github.com/zolrath/obsidian-auto-link-title).
+Note that some pages (e.g. SPAs) may not include the full title in their HTML. Exceptions are handled on a best-case basis, and contributions to add new handlers are very welcome.
 
 #### Page title regexes
 
--- src/try-fetch-title.ts
+++ src/try-fetch-title.ts
@@ -1,30 +1,27 @@
-import { Notice, requestUrl } from "obsidian";
+import { Notice } from "obsidian";
+import { defaultUrlHandler, getSpecialUrlHandler } from "./url-handlers";
 import cleanTitle from "./utils/clean-title";
 
 const tryFetchTitle = async (url: URL, regexes: string[][]) => {
     if (!["http:", "https:"].includes(url.protocol)) return;
-
     new Notice(`Attempting to fetch title from ${url.href}`);
-    const response = await requestUrl({
-        url: url.href,
-        headers: {
-            Accept: "text/html",
-            // assume title is in first 8kb
-            Range: "bytes=0-8000",
-        },
-    });
-    if (!response.headers["content-type"].includes("text/html"))
-        throw new Error(
-            `Invalid content type for ${url.href}: ${response.headers["content-type"]}`
-        );
+
+    let title: string | undefined;
+    const handler = getSpecialUrlHandler(url);
+    if (handler) {
+        try {
+            title = await handler(url);
+        } catch (_) {
+            // fall back to default on failure
+        }
+    }
+    title ??= await defaultUrlHandler(url);
+
     const regex =
         regexes.find(([pageRegex]) =>
             new RegExp(pageRegex).test(url.href)
         )?.[1] || "";
-    return cleanTitle(
-        new DOMParser().parseFromString(response.text, "text/html").title,
-        regex
-    );
+    return cleanTitle(title, regex);
 };
 
 export default tryFetchTitle;
