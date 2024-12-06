eea0b41d4dff43a6490fc19a82d1bc100d39bc47
Jose Alvarez <jealv@icloud.com>
2024-12-06 11:38:21 -0500

    chore: improve version-bump script until I figure out CI

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,42 @@
+import { execSync } from "child_process";
 import { readFileSync, writeFileSync } from "fs";
+import prettier from "prettier";
 
 const targetVersion = process.env.npm_package_version;
 
-// read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
-const { minAppVersion } = manifest;
-manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+(async () => {
+    const hasNewVersion =
+        execSync("git diff --name-only package.json").toString().length > 0;
+    if (!hasNewVersion) {
+        throw new Error("no new version");
+    }
+    const prettierConfig = await prettier.resolveConfig();
 
-// update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+    // read minAppVersion from manifest.json and bump version to target version
+    let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
+    const { minAppVersion } = manifest;
+    manifest.version = targetVersion;
+    writeFileSync(
+        "manifest.json",
+        await prettier.format(JSON.stringify(manifest), {
+            parser: "json",
+            ...prettierConfig,
+        }),
+    );
+
+    // update versions.json with target version and minAppVersion from manifest.json
+    let versions = JSON.parse(readFileSync("versions.json", "utf8"));
+    versions[targetVersion] = minAppVersion;
+    writeFileSync(
+        "versions.json",
+        await prettier.format(JSON.stringify(versions), {
+            parser: "json",
+            ...prettierConfig,
+        }),
+    );
+
+    execSync("npm install && npm run build");
+    execSync(
+        `git add package.json package-lock.json manifest.json versions.json && git commit -m 'chore: ${targetVersion}'`,
+    );
+})();
