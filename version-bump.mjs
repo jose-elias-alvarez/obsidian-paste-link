312d38c38e71d71b2425b8b919cae45415f11ab7
Jose Alvarez <jealv@icloud.com>
2024-12-08 16:06:34 -0500

    chore: fix change check logic

--- version-bump.mjs
+++ version-bump.mjs
@@ -5,11 +5,6 @@ import prettier from "prettier";
 const targetVersion = process.env.npm_package_version;
 
 (async () => {
-    const hasNewVersion =
-        execSync("git diff --name-only package.json").toString().length > 0;
-    if (!hasNewVersion) {
-        throw new Error("no new version");
-    }
     const prettierConfig = await prettier.resolveConfig();
 
     // read minAppVersion from manifest.json and bump version to target version
@@ -21,7 +16,7 @@ const targetVersion = process.env.npm_package_version;
         await prettier.format(JSON.stringify(manifest), {
             parser: "json",
             ...prettierConfig,
-        })
+        }),
     );
 
     // update versions.json with target version and minAppVersion from manifest.json
@@ -32,12 +27,16 @@ const targetVersion = process.env.npm_package_version;
         await prettier.format(JSON.stringify(versions), {
             parser: "json",
             ...prettierConfig,
-        })
+        }),
     );
 
     execSync("npm install && npm run build");
+    const hasChanges = execSync("git status --porcelain").toString().length > 0;
+    if (!hasChanges) {
+        throw new Error("no changes");
+    }
     execSync(
-        `git add package.json package-lock.json manifest.json versions.json && git commit -m 'chore: ${targetVersion}'`
+        `git add package.json package-lock.json manifest.json versions.json && git commit -m 'chore: ${targetVersion}'`,
     );
     execSync(`git tag -a ${targetVersion} -m ${targetVersion}`);
 })();
