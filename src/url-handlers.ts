daad2c4db83f96a19638d268e619d078af4f160b
Jose Alvarez <jealv@icloud.com>
2025-01-29 07:55:50 -0500

    fix: remove range restriction from default handler
    I've seen this break title fetching on GitHub, and I think it's more
    trouble than it's worth.

--- src/url-handlers.ts
+++ src/url-handlers.ts
@@ -33,15 +33,13 @@ export const defaultUrlHandler = async (url: URL) => {
         url: url.href,
         headers: {
             Accept: "text/html",
-            // assume title is in first 8kb
-            Range: "bytes=0-8000",
             // pretend to be desktop chrome to increase odds of getting a full title
             "User-Agent": `Mozilla/5.0 (${
                 Platform.isMacOS
                     ? "Macintosh; Intel Mac OS X 10_15_7"
                     : Platform.isWin
-                    ? "Windows NT 10.0; Win64; x64"
-                    : "X11; Linux x86_64"
+                      ? "Windows NT 10.0; Win64; x64"
+                      : "X11; Linux x86_64"
             }) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36`,
         },
     });
